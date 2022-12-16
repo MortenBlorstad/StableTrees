038a4f57ce10772f907beee031cd73f2d9abb804
Morten Blørstad <MBlorstad@gmail.com>
2022-12-16 10:40:12 +0100

    latest

--- python/examples/Tree_examples2.py
+++ python/examples/Tree_examples2.py
@@ -6,6 +6,9 @@ from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import GridSearchCV
 import sys
 import os
+from warnings import simplefilter
+# ignore all future warnings
+simplefilter(action='ignore', category=FutureWarning)
 cur_file_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(cur_file_path + '\\..\\..\\cpp\\build\\Release\\')
 sys.path.append(cur_file_path + '\\..')
@@ -35,15 +38,13 @@ for ds,target, feature in zip(datasets,targets, features):
     X = data.drop(target, axis=1).to_numpy()
     X12, X_test, y12, y_test = train_test_split(X,y, test_size=0.3, random_state=0)
     X1, X2, y1, y2 = train_test_split(X12,y12, test_size=0.5, random_state=0)
-    clf.fit(X1,y1)
-    params = clf.best_params_
-    print(params)
+    
     # initial model 
     models = {  
-                "baseline": BaseLineTree(**params),
-                "sklearn": sklearnBase(**params,random_state=0),
-                "method1":StableTree1(**params,delta=0.0001),
-                "method2":StableTreeM2(**params),
+                "baseline": BaseLineTree(),
+                "sklearn": sklearnBase(),
+                "method1":StableTree1(),
+                "method2":StableTreeM2(),
         }
     stability = {name:[] for name in models.keys()}
     standard_stability = {name:[] for name in models.keys()}
@@ -57,6 +58,15 @@ for ds,target, feature in zip(datasets,targets, features):
         X_test,y_test = X[test_index],y[test_index]
         X1,X2,y1,y2 =  train_test_split(X_12, y_12, test_size=0.5, random_state=SEED)
         i+=1
+        clf.fit(X1,y1)
+        params = clf.best_params_
+        print(params)
+        models = {  
+                "baseline": BaseLineTree(**params),
+                "sklearn": sklearnBase(**params,random_state=0),
+                "method1":StableTree1(**params,delta=0.0001),
+                "method2":StableTreeM2(**params),
+        }
         for name, model in models.items():
             model.fit(X1,y1)
 
@@ -75,18 +85,20 @@ for ds,target, feature in zip(datasets,targets, features):
             stability[name].append(np.std(np.log((pred1+EPSILON)/(pred2+EPSILON))))
             standard_stability[name].append(np.mean(abs(pred1- pred2)))
         
-        #     if (iteration) % 10 ==0:
-        #         print(f"{iteration}/{50}, {name}")
-        #         print(f"train - mse: {np.mean(train_mse[name]):.3f}, stability: {np.mean(train_stability[name]):.3f}, standard stability: {np.mean(train_standard_stability[name]):.3f}")
-        #         print(f"test - mse: {np.mean(mse[name]):.3f}, stability: {np.mean(stability[name]):.3f}, standard stability: {np.mean(standard_stability[name]):.3f}")
-        # iteration+=1
 
     print(ds)
+    train_mse_denom = np.mean(train_mse["baseline"])
+    train_S1_denom = np.mean(train_stability["baseline"])
+    train_S2_denom = np.mean(train_standard_stability["baseline"])
+    mse_denom = np.mean(mse["baseline"])
+    S1_denom = np.mean(stability["baseline"])
+    S2_denom = np.mean(standard_stability["baseline"])
+
     for name in models.keys():
         print("="*80)
         print(f"{name}")
-        print(f"train - mse: {np.mean(train_mse[name]):.3f}, stability: {np.mean(train_stability[name]):.3f}, standard stability: {np.mean(train_standard_stability[name]):.3f}")
-        print(f"test - mse: {np.mean(mse[name]):.3f}, stability: {np.mean(stability[name]):.3f}, standard stability: {np.mean(standard_stability[name]):.3f}")
+        print(f"train - mse: {np.mean(train_mse[name])/train_mse_denom:.3f}, stability: {np.mean(train_stability[name])/train_S1_denom:.3f}, standard stability: {np.mean(train_standard_stability[name])/train_S2_denom:.3f}")
+        print(f"test - mse: {np.mean(mse[name]):.3f}({np.mean(mse[name])/mse_denom:.3f}), stability: {np.mean(stability[name]):.3f}({np.mean(stability[name])/S1_denom:.3f}), standard stability: : {np.mean(standard_stability[name]):.3f}({np.mean(standard_stability[name])/S2_denom:.3f})")
         print("="*80)
     print()
     
